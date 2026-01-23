8037d534399455d5cf439f32125acef2b5175aec
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-01-23 09:01:18 -0600

    2025 Day 2 Part 2

--- 2025/src/Day2/2.2.logic.ts
+++ 2025/src/Day2/2.2.logic.ts
@@ -1,3 +1,21 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
-}
+import { Coordinate } from '../utils/interfaces/coordinate';
+import { add, multiply, divide } from './2.1.logic';
+
+export const shouldEngrave = (coordinate: Coordinate): boolean => {
+    const maxCycles = 100;
+    let result: Coordinate = { x: 0, y: 0 };
+
+    for (let i = 1; i <= maxCycles; i++) {
+        const productOfItself = multiply(result, result);
+        const divBy100k = divide(productOfItself, { x: 100000, y: 100000 });
+        result = add(divBy100k, coordinate);
+
+        if (result.x > 1000000 || result.y > 1000000 || result.x < -1000000 || result.y < -1000000) {
+            console.log(`The coordinate ${coordinate.x},${coordinate.y} should not be engraved, exited at cycle ${i}.`);
+            return false;
+        }
+    }
+
+    console.log(`The coordinate ${coordinate.x},${coordinate.y} should be engraved.`);
+    return true;
+};
--- 2025/src/Day2/2.2.test.ts
+++ 2025/src/Day2/2.2.test.ts
@@ -1,18 +1,46 @@
 import { describe, expect, test } from '@jest/globals';
-import { add } from './2.2.logic';
+import { shouldEngrave } from './2.2.logic';
 
 describe('Day 2 - Part 2', () => {
-    describe(`When the add function is called...`, () => {
-        test(`with 1 and 2, it should return 3`, () => {
-            expect(add(1, 2)).toBe(3);
+    describe(`When the shouldEngrave function is called...`, () => {
+        test(`It should return false when given { x: 35460, y: -64910}`, () => {
+            expect(shouldEngrave({ x: 35460, y: -64910 })).toBe(false);
         });
 
-        test(`with 2 and 3, it should return 5`, () => {
-            expect(add(2, 3)).toBe(5);
+        test(`It should return false when given { x: 35470, y: -64910}`, () => {
+            expect(shouldEngrave({ x: 35470, y: -64910 })).toBe(false);
         });
 
-        test(`with 3 and 4, it should return 7`, () => {
-            expect(add(3, 4)).toBe(7);
+        test(`It should return false when given { x: 35480, y: -64910 }`, () => {
+            expect(shouldEngrave({ x: 35480, y: -64910 })).toBe(false);
+        });
+
+        test(`It should return false when given { x: 35680, y: -64850 }`, () => {
+            expect(shouldEngrave({ x: 35680, y: -64850 })).toBe(false);
+        });
+
+        test(`It should return false when given { x: 35630, y: -64830 }`, () => {
+            expect(shouldEngrave({ x: 35630, y: -64830 })).toBe(false);
+        });
+
+        test(`It should return true when given { x: 35630, y: -64880}`, () => {
+            expect(shouldEngrave({ x: 35630, y: -64880})).toBe(true);
+        });
+
+        test(`It should return true when given { x: 35630, y: -64870 }`, () => {
+            expect(shouldEngrave({ x: 35630, y: -64870 })).toBe(true);
+        });
+
+        test(`It should return true when given { x: 35640, y: -64860 }`, () => {
+            expect(shouldEngrave({ x: 35640, y: -64860 })).toBe(true);
+        });
+
+        test(`It should return true when given { x: 36230, y: -64270 }`, () => {
+            expect(shouldEngrave({ x: 36230, y: -64270 })).toBe(true);
+        });
+
+        test(`It should return true when given { x: 36250, y: -64270 }`, () => {
+            expect(shouldEngrave({ x: 36250, y: -64270 })).toBe(true);
         });
     });
 });
