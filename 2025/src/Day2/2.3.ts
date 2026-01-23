15bb6b07b9870e7b947a789854348414f449cb35
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-01-23 09:03:27 -0600

    2025 Day 2 Part 3

--- 2025/src/Day2/2.3.ts
+++ 2025/src/Day2/2.3.ts
@@ -1,22 +1,54 @@
-import { add } from './2.3.logic';
+import { add } from './2.1.logic';
+import { shouldEngrave } from './2.2.logic';
+import { Coordinate } from '../utils/interfaces/coordinate';
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+let startingCoordinate: Coordinate = null;
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for Everybody Codes 2025!`);
+    const finalCoordinate = add(startingCoordinate, { x: 1000, y: 1000 });
+    const granularity = 1001;
+
+    const diffX = finalCoordinate.x - startingCoordinate.x;
+    const diffY = finalCoordinate.y - startingCoordinate.y;
+
+    const xSteps = diffX / granularity > 0 ? Math.ceil(diffX / granularity) : Math.floor(diffX / granularity);
+    const ySteps = diffY / granularity > 0 ? Math.ceil(diffY / granularity) : Math.floor(diffY / granularity);
+
+    // Setup an array of arrays of coordinates of size xSteps * ySteps
+    const engravings: boolean[][] = [];
+
+    for (let y = 0; y < granularity; y++) {
+        engravings[y] = [];
+        for (let x = 0; x < granularity; x++) {
+            engravings[y].push(shouldEngrave({ x: startingCoordinate.x + x * xSteps, y: startingCoordinate.y + y * ySteps }));
+        }
+    }
+
+    for (let y = 0; y < granularity; y++) {
+        let line = '';
+        for (let x = 0; x < granularity; x++) {
+            line += engravings[y][x] ? 'x' : '.';
+        }
+        console.log(line);
+    }
+
+    console.log(`The number of engravings is ${engravings.flat().filter(Boolean).length}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+    const parts = line.split(',');
+    startingCoordinate = {
+        x: parseInt(parts[0].replace('A=[', '')),
+        y: parseInt(parts[1].replace(']', '')),
+    };
 };
 
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(test ? './test.txt' : './input.txt')
+    input: require('fs').createReadStream(test ? './test3.txt' : './input3.txt')
 });
 
 lineReader.on('line', (line) => {
