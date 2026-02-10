b5d8a8934c557f51d3ccc2602ab7464ce56f970d
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-02-10 07:19:11 -0600

    2024 Day 7 Parts 1 and 2

--- 2024/src/Day7/7.1.ts
+++ 2024/src/Day7/7.1.ts
@@ -1,18 +1,60 @@
-import { add } from './7.1.logic';
-
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+interface Result {
+    id: string;
+    energies: number[];
+    totalEnergy: number;
+}
+
+
+const chariots: Map<string, string[]> = new Map();
+const results: Result[] = [];
+
+const MAX_ROUNDS = 10;
+const STARTING_ENERGY = 10;
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for Everybody Codes 2024!`);
+    chariots.forEach((moves, id) => {
+        let currentEnergy = STARTING_ENERGY;
+        let energiesByRound: number[] = [];
+
+        for (let round = 0; round < MAX_ROUNDS; round++) {
+            const move = moves[round % moves.length];
+
+            if (move === '+') {
+                currentEnergy++;
+            } else if (move === '-' && currentEnergy > 0) {
+                currentEnergy--;
+            } else if (move === '=') {
+                // Do nothing
+            }
+
+            energiesByRound.push(currentEnergy);
+        }
+
+        results.push({
+            id,
+            energies: energiesByRound,
+            totalEnergy: energiesByRound.reduce((a, b) => a + b, 0)
+        });
+    });
+
+    if (debug) {
+        results.forEach((result) => {
+            console.log(`${result.id}: ${result.energies.join(',')} = ${result.totalEnergy}`);
+        });
+    }
+
+    results.sort((a, b) => b.totalEnergy - a.totalEnergy);
+    console.log(`The order of action plans is ${results.map((result) => result.id).join('')}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+   const parts = line.split(':');
+   const moves = parts[1].split(',');
+   chariots.set(parts[0], moves);
 };
 
 var lineReader = require('readline').createInterface({
--- 2024/src/Day7/7.2.ts
+++ 2024/src/Day7/7.2.ts
@@ -1,18 +1,119 @@
-import { add } from './7.2.logic';
-
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+interface Result {
+    id: string;
+    energies: number[];
+    totalEnergy: number;
+}
+
+
+const chariots: Map<string, string[]> = new Map();
+const track: string[] = [];
+const trackActions: string[] = [];
+const results: Result[] = [];
+
+const MAX_ROUNDS = 10;
+const STARTING_ENERGY = 10;
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for Everybody Codes 2024!`);
+    track[0].split('').forEach((char, index) => {
+        if (char !== 'S') {
+            trackActions.push(char);
+        }
+    });
+
+    // Now get the right side actions
+    for (let i = 1; i < track.length - 1; i++) {
+        trackActions.push(track[i][track[i].length - 1]);
+    };
+
+    // Now invert the final track actions and push them in
+    track[track.length - 1].split('').reverse().forEach((char) => {
+        trackActions.push(char);
+    });
+
+    // Now go back up the right side actions
+    for (let i = track.length - 2; i > 0; i--) {
+        trackActions.push(track[i][0]);
+    };
+
+    trackActions.push('S');
+
+    if (debug) {
+        console.log(trackActions.join(''));
+    }
+
+    chariots.forEach((moves, id) => {
+        if (debug) {
+            console.log(`========== Calculating energy for ${id} ==========`);
+        }
+        let currentEnergy = STARTING_ENERGY;
+        let energiesByRound: number[] = [];
+        let stepsSoFar = 0;
+
+        for (let round = 0; round < MAX_ROUNDS; round++) {
+            for (let step = 0; step < trackActions.length; step++) {
+                const move = moves[stepsSoFar % moves.length];
+                const override = trackActions[step];
+
+                if (override === 'S' || override === '=') {
+                    if (move === '+') {
+                        currentEnergy++;
+                    } else if ((move === '-') && (currentEnergy > 0)) {
+                        currentEnergy--;
+                    } else if (move === '=') {
+                        // Do nothing
+                    }
+                } else if (override === '+') {
+                    currentEnergy++;
+                } else if ((override === '-') && (currentEnergy > 0)) {
+                    currentEnergy--;
+                }
+
+                if (energiesByRound.length <= round) {
+                    if (debug) {
+                        console.log(`Adding energy for round ${round} = (override ${override}, move ${move}) = ${currentEnergy}`);
+                    }
+                    energiesByRound.push(currentEnergy);
+                } else {
+                    energiesByRound[round] += currentEnergy;
+                    if (debug) {
+                        console.log(`Adding energy for round ${round} = (override ${override}, move ${move}) = ${currentEnergy}, total = ${energiesByRound[round]}`);
+                    }
+                }
+
+                stepsSoFar++;
+            }
+        }
+
+        results.push({
+            id,
+            energies: energiesByRound,
+            totalEnergy: energiesByRound.reduce((a, b) => a + b, 0)
+        });
+    });
+
+    results.sort((a, b) => b.totalEnergy - a.totalEnergy);
+
+    if (debug) {
+        results.forEach((result) => {
+            console.log(`${result.id}: ${result.energies.join(',')} = ${result.totalEnergy}`);
+        });
+    }
+
+    console.log(`The order of action plans is ${results.map((result) => result.id).join('')}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+    if (line.indexOf(':') !== -1) {
+        const parts = line.split(':');
+        const moves = parts[1].split(',');
+        chariots.set(parts[0], moves);
+    } else if (line.length > 0) {
+        track.push(line);
+    }
 };
 
 var lineReader = require('readline').createInterface({
