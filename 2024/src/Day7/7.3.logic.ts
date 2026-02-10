907ffc5c69286e1df04459551af86c9a97f596e5
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-02-10 08:43:40 -0600

    2024 Day 7 Part 3

--- 2024/src/Day7/7.3.logic.ts
+++ 2024/src/Day7/7.3.logic.ts
@@ -1,3 +1,130 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+export interface TrackInput {
+    value: string;
+    visited: boolean;
+    neighbors: TrackInput[];
+}
+
+export const MAX_ROUNDS = 2024;
+export const STARTING_ENERGY = 10;
+
+export const connectTrackInputs = (inputs: TrackInput[][]) => {
+    for (let row = 0; row < inputs.length; row++) {
+        for (let column = 0; column < inputs[row].length; column++) {
+            const current = inputs[row][column];
+
+            if (current === null) {
+                continue;
+            }
+
+            // Add North neighbor
+            if ((row > 0) && (inputs[row - 1][column] !== null)) {
+                current.neighbors.push(inputs[row - 1][column]);
+            }
+
+            // Add South neighbor
+            if ((row < inputs.length - 1) && (inputs[row + 1][column] !== null)) {
+                current.neighbors.push(inputs[row + 1][column]);
+            }
+
+            // Add West neighbor
+            if ((column > 0) && (inputs[row][column - 1] !== null)) {
+                current.neighbors.push(inputs[row][column - 1]);
+            }
+
+            // Add East neighbor
+            if ((column < inputs[row].length - 1) && (inputs[row][column + 1] !== null)) {
+                current.neighbors.push(inputs[row][column + 1]);
+            }
+        }
+    }
+}
+
+export const determineTrackActionsInOrder = (inputs: TrackInput[][]): string[] => {
+    let trackActions: string[] = [];
+    let countTrackActions = 0;
+
+    // Start with the first position after S
+    const queue = [inputs[0][1]];
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+
+        countTrackActions++;
+        if (current.value !== 'S') {
+            trackActions.push(current.value);
+        }
+
+        current.neighbors.forEach((neighbor) => {
+            if ((neighbor !== undefined) && (!neighbor.visited) && (neighbor.value !== 'S')) {
+                queue.push(neighbor);
+            }
+        });
+
+        current.visited = true;     
+    }
+
+    // Now add the S
+    trackActions.push('S');
+
+    return trackActions;
+}
+
+export const runTheTrack = (trackActions: string[], moves: string[]): number => {
+    let currentEnergy = STARTING_ENERGY;
+    let energiesByRound: number[] = [];
+    let stepsSoFar = 0;
+
+    for (let round = 0; round < MAX_ROUNDS; round++) {
+        for (let step = 0; step < trackActions.length; step++) {
+            const move = moves[stepsSoFar % moves.length];
+            const override = trackActions[step];
+
+            if (override === 'S' || override === '=') {
+                if (move === '+') {
+                    currentEnergy++;
+                } else if ((move === '-') && (currentEnergy > 0)) {
+                    currentEnergy--;
+                } else if (move === '=') {
+                    // Do nothing
+                }
+            } else if (override === '+') {
+                currentEnergy++;
+            } else if ((override === '-') && (currentEnergy > 0)) {
+                currentEnergy--;
+            }
+
+            if (energiesByRound.length <= round) {
+                energiesByRound.push(currentEnergy);
+            } else {
+                energiesByRound[round] += currentEnergy;
+            }
+
+            stepsSoFar++;
+        }
+    }
+
+    return energiesByRound.reduce((a, b) => a + b, 0)
+}
+
+// Super cool, never used a generator before in TS.
+export function* permute(permutation: string[]): Generator<string[]> {
+    var length = permutation.length,
+        c = Array(length).fill(0),
+        i = 1, k, p;
+
+    yield permutation.slice();
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i];
+            p = permutation[i];
+            permutation[i] = permutation[k];
+            permutation[k] = p;
+            ++c[i];
+            i = 1;
+            yield permutation.slice();
+        } else {
+            c[i] = 0;
+            ++i;
+        }
+    }
 }
--- 2024/src/Day7/7.3.ts
+++ 2024/src/Day7/7.3.ts
@@ -1,28 +1,94 @@
-import { add } from './7.3.logic';
+import { TrackInput, connectTrackInputs, determineTrackActionsInOrder, runTheTrack, permute } from './7.3.logic';
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+interface Result {
+    id: string;
+    energies: number[];
+    totalEnergy: number;
+}
+
+const trackInput: TrackInput[][] = [];
+let countWinningStrategies = 0;
+
+let opponentStrategy: string[] = [];
+const checkecStrategies: Map<string, boolean> = new Map();
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for Everybody Codes 2024!`);
+    connectTrackInputs(trackInput);
+
+    const trackActions = determineTrackActionsInOrder(trackInput);
+
+    if (debug) {
+        console.log(`There are ${trackActions.length} track actions`);
+        console.log(`The track actions are ${trackActions.join('')}`);
+    }
+
+    const opponentTotalEnergy = runTheTrack(trackActions, opponentStrategy);
+    console.log(`The opponent total energy is ${opponentTotalEnergy}`);
+
+    for (const permutation of permute(['+', '+', '+', '+', '+', '-', '-', '-', '=', '=', '='])) {
+        let hash = permutation.join('');
+        if (!checkecStrategies.has(hash)) {
+            checkecStrategies.set(hash, true);
+
+            if (debug) {
+                console.log(`Evaluating permutation: ${permutation.join('')}`);
+            }
+            const totalEnergy = runTheTrack(trackActions, permutation);
+            if (totalEnergy > opponentTotalEnergy) {
+                countWinningStrategies++;
+                if (debug) {
+                    console.log(`Winning strategy found`);
+                }
+            }
+        }
+    }
+
+    console.log(`There are ${countWinningStrategies} winning strategies`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+    const parts = line.split(':');
+    opponentStrategy = parts[1].split(',');
+};
+
+const parseTrackLine = (line: string) => {
+    if (debug) {
+        console.log(`Parsing track line: ${line}`);
+    }
+
+    let row: TrackInput[] = [];
+    line.split('').forEach((char) => {
+        if (char !== ' ') {
+            row.push({ value: char, visited: false, neighbors: [] });
+        } else {
+            row.push(null);
+        }
+    });
+    trackInput.push(row);
 };
 
 var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(test ? './test3.txt' : './input3.txt')
 });
 
+
+
 lineReader.on('line', (line) => {
     parseLine(line);
 }).on('close', () => {
-    execute();
+    var trackReader = require('readline').createInterface({
+        input: require('fs').createReadStream('./track3.txt')
+    });
+
+    trackReader.on('line', (line) => {
+        parseTrackLine(line);
+    }).on('close', () => {
+        execute();
+    });
 });
 
 export {};
