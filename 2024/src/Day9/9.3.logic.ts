3e0651b290085ab24f7e408cfef25a47f217f944
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-02-13 07:46:10 -0600

    2024 Day 9 Parts 1 and 2 working, 3 has an issue somewhere

--- 2024/src/Day9/9.3.logic.ts
+++ 2024/src/Day9/9.3.logic.ts
@@ -1,3 +1,56 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+interface PopulateCacheFromStampsParams {
+    stamps: number[];
+    rootValue: number;
+    maxValue: number;
+    cache: Map<number, number>;
+}
+
+interface QueuedItem {
+    value: number;
+    depth: number;
+}
+
+export const populateCacheFromStampsParams = (params: PopulateCacheFromStampsParams): void => {
+    const { stamps, rootValue, maxValue, cache } = params;
+    const queue: QueuedItem[] = [{
+        value: rootValue,
+        depth: 1
+    }];
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+
+        for (const stamp of stamps) {
+            const newNodeValue = current.value + stamp;
+            const depth = current.depth + 1;
+
+            if (!cache.has(newNodeValue)) {
+                cache.set(newNodeValue, depth);
+
+                if (newNodeValue <= maxValue) {
+                    queue.push({
+                        value: newNodeValue,
+                        depth: depth
+                    });
+                }
+            } else if (cache.has(newNodeValue) && cache.get(newNodeValue) > depth) {
+                cache.set(newNodeValue, depth);
+            }
+        }
+    }
+}
+
+interface GenerateCacheFromStampsParams {
+    stamps: number[];
+    maxValue: number;
+}
+
+export const generateCacheFromStamps = (params: GenerateCacheFromStampsParams): Map<number, number> => {
+    const { stamps, maxValue } = params;
+    const cache = new Map<number, number>();
+
+    for (const stamp of stamps) {
+        populateCacheFromStampsParams({ stamps, rootValue: stamp, maxValue, cache });
+    }
+    return cache;
 }
--- 2024/src/Day9/9.3.ts
+++ 2024/src/Day9/9.3.ts
@@ -1,18 +1,63 @@
-import { add } from './9.3.logic';
-
+import { generateCacheFromStamps } from './9.3.logic';
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+const sparkballs: number[] = [];
+const stamps: number[] = [101, 100, 75, 74, 50, 49, 38, 37, 30, 25, 24, 20, 16, 15, 10, 5, 3, 1];
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for Everybody Codes 2024!`);
+    let totalBeetlesRequired = 0;
+    sparkballs.sort((a, b) => b - a);
+    const highestSparkball = sparkballs[0];
+
+    console.log(`Highest sparkball: ${highestSparkball}`);
+
+    const halfHighestSparkballPlus100 = Math.ceil(highestSparkball / 2) + 100;
+    console.log(`Half highest sparkball plus 100: ${halfHighestSparkballPlus100}`);
+
+    const cache = generateCacheFromStamps({ stamps, maxValue: halfHighestSparkballPlus100 });
+    if (debug) {
+        console.log(`Cache has ${cache.size} entries`);
+        cache.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
+    }
+
+    sparkballs.forEach((sparkball) => {
+        let shortestPathLength = Infinity;
+        const halfSparkball = Math.floor(sparkball / 2);
+
+        for (let i = halfSparkball - 100; i <= halfSparkball + 100; i++) {
+            let pathLength = cache.get(i);
+
+            if (pathLength < Infinity) {
+                const remainder = sparkball - i;
+
+                if (halfSparkball - remainder <= 100) {
+                    let remainderPathLength = cache.get(remainder);
+
+                    if (remainderPathLength < Infinity) {
+                        const totalPathLength = pathLength + remainderPathLength;
+                        if (totalPathLength < shortestPathLength) {
+                            shortestPathLength = totalPathLength;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (debug) {
+            console.log(`Shortest path length for sparkball ${sparkball}: ${shortestPathLength}`);
+        }
+        totalBeetlesRequired += shortestPathLength;
+    });
+
+    console.log(`Total beetles required: ${totalBeetlesRequired}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+   sparkballs.push(Number(line));
 };
 
 var lineReader = require('readline').createInterface({
